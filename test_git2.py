928a1005b06d467490c4c72ffc0432322044676d
Tuan <tuanstarkpd@gmail.com>
2023-01-14 16:14:22 +0700

    update cmt in file

--- test_git2.py
+++ test_git2.py
@@ -1,1 +1,6 @@
-# giống kiểu ta theo dõi 1 file(staging area) nếu file đó mà thay đổi gì thì lưu lại(commit)
+# giống kiểu ta theo dõi 1 file(staging area) nếu file đó mà thay đổi gì muốn lưu lại thì lưu lại(commit)
+#sau mỗi lần thay đổi file, muốn lưu vào kho thì phải theo dõi rồi commit thì nó mới lưu file hiện tại mới dc thay đổi cái j
+#kho lưu trữ cá nhân lưu trữ những gì mà cá nhân thay đổi, kho lưu trữ sever lưu trữ thay đổi của cả nhóm
+#kho lưu trữ để lưu các bản sao thay đổi của các file
+#thay đổi mà ko commit thì nó k lưu vào kho lư trữ
+#mỗi ng lm 1 file rồi lưu vào chung 1 thư mục, nhiều ng lm trên 1 file sẽ gây xunh đột, phải sửa lần lượt
